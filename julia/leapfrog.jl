3b11c123edb5206c70ab7313f1e274d01b6124b2
Sujeet Akula <sujeet@freeboson.org>
2017-03-27 14:36:55 +1100

    typo in tmax. just go 1 iteration.

--- julia/leapfrog.jl
+++ julia/leapfrog.jl
@@ -50,8 +50,8 @@ function main(;dt::Real=0.08, tmax::Real=3.6525e8)
     println("Positions:", x)
 end
 
-println("Running up to tmax=10 to compile...")
-@time main(tmax=1e2)
+println("Running up to tmax=1.0 to compile...")
+@time main(tmax=1.0)
 println("Running full benchmark...")
 @time main()
 
